2c540547b2e7d8fc56ffe9b77f104c777e971491
Andrey Kruglik <isekaiguy@protonmail.com>
2021-10-21 20:37:54 +0700

    added class App with setStyle and getPosition methods

--- script.js
+++ script.js
@@ -23,68 +23,99 @@ const form = document.querySelector(".form"),
   inputCadence = document.querySelector(".form__input_cadence"),
   inputElevation = document.querySelector(".form__input_elevation"),
   popupQuestion = document.querySelector(".popup-question"),
-  chooseWalking = document.querySelector(".choose-transport__walk"),
-  chooseCycling = document.querySelector(".choose-transport__bycicle"),
-  sidebar = document.querySelector(".sidebar");
+  images = popupQuestion.querySelectorAll("img"),
+  sidebar = document.querySelector(".sidebar"),
+  root = document.documentElement;
 
-let root = document.documentElement;
-let transportType = "";
+class App {
+  #coords;
+  #map;
+
+  constructor() {
+    this._getPosition();
+  }
+
+  _setStyle(type) {
+    if (type === "walking") {
+      sidebar.firstElementChild.setAttribute("src", "logo.png");
+      popupQuestion.style.display = "none";
+      root.style.setProperty("--color-brand", "#00c46954");
+      form.classList.remove("hidden");
+      inputDistance.focus();
+      inputElevation.parentElement.classList.add("form__row_hidden");
+      inputCadence.parentElement.classList.remove("form__row_hidden");
+    } else {
+      sidebar.firstElementChild.setAttribute("src", "bycicle-logo.png");
+      popupQuestion.style.display = "none";
+      root.style.setProperty("--color-brand", "#ffb54577");
+      form.classList.remove("hidden");
+      inputDistance.focus();
+      inputCadence.parentElement.classList.add("form__row_hidden");
+      inputElevation.parentElement.classList.remove("form__row_hidden");
+    }
+  }
+
+  _getPosition() {
+    navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => {
+      alert("Could not get your position!");
+    });
+  }
+
+  _loadMap(position) {
+    const { longitude, latitude } = position.coords;
+
+    this.#coords = [latitude, longitude];
+    this.#map = L.map("map").setView(this.#coords, 13);
+
+    L.marker(this.#coords)
+      .addTo(this.#map)
+      .bindPopup("You are here!")
+      .openPopup();
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on("click", (mapEvent) => {
+      this.#coords = [mapEvent.latlng.lat, mapEvent.latlng.lng];
+    });
+  }
+
+  _showForm() {}
+
+  _newWorkout() {}
+}
+
+const app = new App();
 
 // POPUP QUESTION
-
-chooseWalking.addEventListener("click", () => {
-  transportType = "walking";
-  sidebar.firstElementChild.setAttribute("src", "logo.png");
-  popupQuestion.style.display = "none";
-  root.style.setProperty("--color-brand", "#00c46954");
-  form.classList.remove("hidden");
-  inputDistance.focus();
-});
-
-chooseCycling.addEventListener("click", () => {
-  transportType = "cycling";
-  sidebar.firstElementChild.setAttribute("src", "bycicle-logo.png");
-  popupQuestion.style.display = "none";
-  root.style.setProperty("--color-brand", "#ffb54577");
-  form.classList.remove("hidden");
-  inputDistance.focus();
+images.forEach((image) => {
+  image.addEventListener("click", (e) => {
+    let transportType = e.target.getAttribute("alt");
+    app._setStyle(transportType);
+  });
 });
 
 // MAP
 
-navigator.geolocation.getCurrentPosition(
-  (position) => {
-    const { longitude, latitude } = position.coords;
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-    let coords = [latitude, longitude];
-    const map = L.map("map").setView(coords, 13);
+//   inputDistance.value =
+//     inputDuration.value =
+//     inputElevation.value =
+//     inputCadence.value =
+//       "";
 
-    L.marker(coords).addTo(map).bindPopup("You are here!").openPopup();
-    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
-  },
-  () => {
-    alert("Could not get your position!");
-  }
-);
-
-form.addEventListener("submit", () => {
-  L.marker(coords).addTo(map).bindPopup("You are here!").openPopup();
-
-  map.on("click", (mapEvent) => {
-    coords = [mapEvent.latlng.lat, mapEvent.latlng.lng];
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup(
-        L.popup({
-          maxWidth: 250,
-          minWidth: 100,
-          className: "running-popup",
-        })
-      )
-      .setPopupContent("Hurry!")
-      .openPopup();
-  });
-});
+//   L.marker(coords)
+//     .addTo(map)
+//     .bindPopup(
+//       L.popup({
+//         maxWidth: 250,
+//         minWidth: 100,
+//         className: "running-popup",
+//       })
+//     )
+//     .setPopupContent("Hurry!")
+//     .openPopup();
+// });
